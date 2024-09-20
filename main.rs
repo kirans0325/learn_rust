5fd4bbb6e3e5a1a0d7e2f7c7060fd2865e21a457
kirans0325 <kirans0325@gmail.com>
2024-09-20 16:07:31 +0530

    main.rs changed

--- main.rs
+++ main.rs
@@ -1,4 +1,15 @@
+// use std::collections::HashMap;
+
+
 fn main(){
-    println!("Hello")
-    
+
+    let val = vec![2,3,4,5];
+    let val_iter = val.iter().filter(|x|*x%2==1).map(|x|x*2);
+   for x in val_iter{
+    println!("vals{} ",x);
+   }
+   
 }
+
+
+
